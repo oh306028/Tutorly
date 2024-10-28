ad10b3b3c848926340fa6a5104157f7a5c89431b
Oskar Hamerla <hamerlao@gmail.com>
2024-10-28 23:43:37 +0100

    refactored the post model; nullable property disabled; models exceiptions added

--- Tutorly.Domain/Models/Post.cs
+++ Tutorly.Domain/Models/Post.cs
@@ -13,6 +13,7 @@ namespace Tutorly.Domain.Models
         public int CurrentStudentAmount => Students.Count;
 
         private readonly List<Student> Students = new();
+        public string Description { get; set; } 
 
         public int TutorId { get; set; }
         public Tutor Tutor { get; set; }
@@ -22,10 +23,19 @@ namespace Tutorly.Domain.Models
 
         public void AddStudent(Student student)
         {
-            if (CurrentStudentAmount < MaxStudentAmount)
-                Students.Add(student);
+            if (CurrentStudentAmount >= MaxStudentAmount)
+               throw new OutOfSpaceException("Current post cannot hold more students");
 
-            throw new OutOfSpaceException("Current post cannot hold more students");
+            Students.Add(student);
+
+        }
+
+        public void DeleteStudent(Student student)
+        {
+            if(!Students.Contains(student))
+                throw new NotFoundException("Cannot delete student that is not a part of current post");
+
+            Students.Remove(student);
 
         }
     }
---
+++ Tutorly.Domain/ModelsExceptions/OutOfSpaceException.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tutorly.Domain.ModelsExceptions
+{
+    public class OutOfSpaceException : Exception
+    {
+        public OutOfSpaceException(string message) : base(message)
+        {
+            
+        }
+    }
+}
