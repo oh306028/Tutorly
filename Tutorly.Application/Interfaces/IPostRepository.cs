ef0637f0690dd0321499ce895636f0edae20506b
Oskar Hamerla <hamerlao@gmail.com>
2024-10-28 23:38:13 +0100

    created irepostirory interface and created the post repository interface

---
+++ Tutorly.Application/Interfaces/IPostRepository.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Tutorly.Domain.Models;
+
+namespace Tutorly.Application.Interfaces
+{
+    public class IPostRepository : IRepository<Post>
+    {
+        public void AddAsync(Post post) 
+        {
+            throw new NotImplementedException();
+        }
+
+        public void DeleteAsync(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IEnumerable<Post>> GetAllAsync()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Post> GetByIdAsync(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void UpdateAsync(Post post)  
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ Tutorly.Application/Interfaces/IRepository.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tutorly.Application.Interfaces
+{
+    public interface IRepository<T> where T : class
+    {
+        Task<IEnumerable<T>> GetAllAsync();
+        Task<T> GetByIdAsync(int id);
+
+        void AddAsync(T entity);    
+
+        void UpdateAsync(T entity); 
+
+        void DeleteAsync(int id);   
+
+    }
+}
