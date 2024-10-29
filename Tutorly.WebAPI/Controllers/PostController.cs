23dea09ffe5985d6b18cdb03adf9201440d9d8d1
Oskar <hamerlao@gmail.com>
2024-10-29 23:04:17 +0100

    Prepared the post controller, retrieiving the data and posting a post for now

---
+++ Tutorly.WebAPI/Controllers/PostController.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Tutorly.Application.Commands;
+using Tutorly.Application.Handlers;
+using Tutorly.Domain.Models;
+
+namespace Tutorly.WebAPI.Controllers
+{
+    [Route("api/posts")]
+    [ApiController]
+    public class PostController : ControllerBase
+    {
+       
+        [HttpGet]
+        public ActionResult<IEnumerable<Post>> GetAll()
+        {
+         
+
+        }
+
+
+        [HttpPost("id")]
+        public ActionResult ApplyForPost([FromRoute] int id)
+        {
+
+        }
+
+
+
+
+    }
+}
