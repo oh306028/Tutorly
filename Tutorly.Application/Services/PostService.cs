93e497c63b922e6150664bf45bf6dc72eca137be
Oskar <hamerlao@gmail.com>
2024-10-31 17:17:44 +0100

    added the post service as a mediator between controller and handlers

---
+++ Tutorly.Application/Services/PostService.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Tutorly.Application.Commands;
+using Tutorly.Application.Dtos;
+using Tutorly.Application.Handlers;
+using Tutorly.Application.Interfaces;
+using Tutorly.Application.Queries;
+using Tutorly.Domain.Models;
+
+namespace Tutorly.Application.Services
+{
+    public interface IPostService
+    {
+        Task<IEnumerable<Post>> GetAll(QueryParams queryParams = null);
+    }
+
+    public class PostService : IPostService
+    {
+        private readonly IHandler<CreatePost> _handler;
+        private readonly IQueryHandler<GetAllPosts, IEnumerable<Post>> _queryHandler;
+        public PostService(IHandler<CreatePost> handler, IQueryHandler<GetAllPosts, IEnumerable<Post>> queryHandler)
+        {
+            _handler = handler;
+            _queryHandler = queryHandler;
+        }
+
+        public async Task<IEnumerable<Post>> GetAll(QueryParams? queryParams = null)
+        {
+            var query = new GetAllPosts(queryParams);
+            var posts = await _queryHandler.HandleAsync(query);
+                
+            return posts;   
+        }
+
+
+    }
+}
