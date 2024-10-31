a9378e4e6501ea70038415c9671c86c44de3e1fe
Oskar <hamerlao@gmail.com>
2024-10-31 17:18:22 +0100

    refactored the post controller, refactored the handlers injections into the service instead

--- Tutorly.WebAPI/Controllers/PostController.cs
+++ Tutorly.WebAPI/Controllers/PostController.cs
@@ -1,7 +1,12 @@
-﻿using Microsoft.AspNetCore.Http;
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Tutorly.Application.Commands;
+using Tutorly.Application.Dtos;
 using Tutorly.Application.Handlers;
+using Tutorly.Application.Interfaces;
+using Tutorly.Application.Queries;
+using Tutorly.Application.Services;
 using Tutorly.Domain.Models;
 
 namespace Tutorly.WebAPI.Controllers
@@ -10,22 +15,24 @@ namespace Tutorly.WebAPI.Controllers
     [ApiController]
     public class PostController : ControllerBase
     {
-       
+        private readonly IPostService _postService;
+
+        public PostController(IPostService postService) 
+        {
+            _postService = postService;
+        }
+
+
         [HttpGet]
-        public ActionResult<IEnumerable<Post>> GetAll()
-        {
-         
-
-        }
-
-
-        [HttpPost("id")]
-        public ActionResult ApplyForPost([FromRoute] int id)
+        public async Task<ActionResult<IEnumerable<Post>>> GetAll([FromBody] QueryParams? queryParams = null)  
         {
 
+            var result = await _postService.GetAll(queryParams);
+            return Ok(result);
+                
         }
-
-
+        
+     
 
 
     }
