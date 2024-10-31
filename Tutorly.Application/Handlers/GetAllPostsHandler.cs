ee134264dac6c7ee798936fa84d9dcccd5851bb4
Oskar <hamerlao@gmail.com>
2024-10-31 16:05:29 +0100

    created IQuery and GetAllPosts query; also added GetAllPostsHandlers for handling this query, inserted the possibility to add filtering

---
+++ Tutorly.Application/Handlers/GetAllPostsHandler.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Tutorly.Application.Interfaces;
+using Tutorly.Application.Queries;
+using Tutorly.Domain.Models;
+
+namespace Tutorly.Application.Handlers
+{
+    public class GetAllPostsHandler : IQueryHandler<GetAllPosts, IEnumerable<Post>>
+    {
+        private readonly IRepository<Post> _postRepository;
+
+        public GetAllPostsHandler(IRepository<Post> postRepository) 
+        {
+            _postRepository = postRepository;       
+        }
+        public async Task<IEnumerable<Post>> HandleAsync(GetAllPosts query)
+        {
+
+            var posts =
+                query.CategoryId is null ?      
+                await _postRepository.GetAllAsync() :           
+                await _postRepository.GetAllAsync(x => x.CategoryId == query.CategoryId);
+
+            return posts;
+
+        }
+    }
+}
---
+++ Tutorly.Application/Queries/GetAllPosts.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Tutorly.Application.Dtos;
+using Tutorly.Application.Interfaces;
+using Tutorly.Domain.Models;
+
+namespace Tutorly.Application.Queries
+{
+    public class GetAllPosts : IQuery<IEnumerable<Post>>
+    {
+        public GetAllPosts(QueryParams? queryParams = null) 
+        {
+            CategoryId = queryParams.CategoryId;    
+        }
+        public int? CategoryId { get; init; }    
+    }
+}
