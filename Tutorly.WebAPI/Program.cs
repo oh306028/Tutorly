630c4cc976c5ad1fde8c2b3ce2ea0bce337f7639
Oskar <hamerlao@gmail.com>
2024-11-01 11:39:36 +0100

    registered the dbContext and send the connection string from appsettings

--- Tutorly.WebAPI/Program.cs
+++ Tutorly.WebAPI/Program.cs
@@ -1,8 +1,17 @@
+using Microsoft.EntityFrameworkCore;
+using Tutorly.Infrastructure;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
 builder.Services.AddControllers();
+
+builder.Services.AddDbContext<TutorlyDbContext>(options =>
+{
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"));
+});
+
 
 var app = builder.Build();
 
