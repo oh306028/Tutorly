02eac4f339d9b8caeec3dd160f912084a899ab08
Oskar <hamerlao@gmail.com>
2024-11-03 21:29:08 +0100

    added the dto for creating posts

---
+++ Tutorly.Application/Dtos/CreatePostDto.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tutorly.Application.Dtos
+{
+    public class CreatePostDto
+    {
+        public int CategoryId { get; set; }
+        public int MaxStudentAmount { get; set; }
+        public int TutorId { get; set; }
+        public string Description { get; set; } 
+    }
+}
