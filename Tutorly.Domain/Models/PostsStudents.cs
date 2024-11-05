bd03b2a0925d4c80e11caeeb40d3ef979b5dc2ba
Oskar <hamerlao@gmail.com>
2024-11-05 21:26:00 +0100

    added a model for join table; m-to-m posts students

---
+++ Tutorly.Domain/Models/PostsStudents.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tutorly.Domain.Models
+{
+    public class PostsStudents
+    {
+        public int PostId { get; set; }
+        public Post Post { get; set; }
+
+
+        public int StudentId { get; set; }
+        public Student Student { get; set; }
+    }
+}
