2bc63ebeb13bc282be9d97f00bdbf02500034937
Oskar <hamerlao@gmail.com>
2024-11-05 23:06:09 +0100

    created the address and dayOftheWeek models

---
+++ Tutorly.Domain/Models/DayOfWeek.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tutorly.Domain.Models
+{
+    public enum DayOfWeek
+    {
+        Monday,
+        Tuesday,
+        Wednseday,
+        Thursday,
+        Friday,
+        Saturday,
+        Sunday
+    }
+}
