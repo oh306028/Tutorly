df05f85751a34af8f8d82f6c4f22c450c15e3334
Oskar <hamerlao@gmail.com>
2024-11-06 15:24:24 +0100

    refactored the join table, added the on restrict deletion for that table

--- Tutorly.Infrastructure/TutorlyDbContext.cs
+++ Tutorly.Infrastructure/TutorlyDbContext.cs
@@ -14,22 +14,76 @@ namespace Tutorly.Infrastructure
         public DbSet<User> Users { get; set; }
         public DbSet<Category> Categories { get; set; }
 
+        public TutorlyDbContext(DbContextOptions options) : base(options)
+        {
+                
+        }
+
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
-            modelBuilder.Entity<User>().HasDiscriminator(r => r.Role)
+            modelBuilder.Entity<User>()
+                .HasDiscriminator(r => r.Role)
                 .HasValue<Tutor>(Role.Tutor)
                 .HasValue<Student>(Role.Student);
 
-            modelBuilder.Entity<Post>()
-                .HasMany(s => s.Students)
-                .WithOne(p => p.Post)
-                .HasForeignKey(k => k.PostId);
 
+            modelBuilder.Entity<Tutor>()
+                .HasMany(p => p.Posts)
+                .WithOne(t => t.Tutor)
+                .OnDelete(DeleteBehavior.Cascade);
+
+            modelBuilder.Entity<PostsStudents>(ps =>
+            {
+                ps.HasKey(ps => new { ps.PostId, ps.StudentId });
+
+                ps.HasOne(c => c.Student)
+                .WithMany(r => r.Posts)
+                .HasForeignKey(k => k.StudentId)
+                .OnDelete(DeleteBehavior.Restrict);
+
+                ps.HasOne(c => c.Post)
+               .WithMany(r => r.Students)
+               .HasForeignKey(k => k.PostId)
+               .OnDelete(DeleteBehavior.Cascade);
+                    
+            });
+               
+          
+            modelBuilder.Entity<Post>()
+                .HasOne(p => p.Address)
+                .WithOne(a => a.Post)
+                .OnDelete(DeleteBehavior.Cascade); 
+
+
+
+            modelBuilder.Entity<Post>()
+                .HasOne(a => a.Address)
+                .WithOne(p => p.Post);
+
+            modelBuilder.Entity<Address>(a =>
+            {
+                a.Property(c => c.City)
+                .IsRequired()
+                .HasMaxLength(15);
+
+                a.Property(s => s.Street)
+                .IsRequired()
+                .HasMaxLength(20);
+
+                a.Property(n => n.Number)
+                .IsRequired()
+                .HasMaxLength(10);
+
+            });
+
+            modelBuilder.Entity<Category>()
+                .Property(n => n.Name)
+                .IsRequired()
+                .HasMaxLength(30);
 
             modelBuilder.Entity<Post>()
                 .HasOne(c => c.Category)
                 .WithOne(p => p.Post);
-
 
 
             modelBuilder.Entity<User>(u =>
