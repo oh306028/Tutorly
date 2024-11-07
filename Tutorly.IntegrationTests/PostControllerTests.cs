b1b1ad40e86b42f4ed78b2645b3fc9c09bec0644
Oskar <hamerlao@gmail.com>
2024-11-07 18:38:13 +0100

    created the integration test project; initiated with GetPosts test

---
+++ Tutorly.IntegrationTests/PostControllerTests.cs
@@ -0,0 +1,29 @@
+﻿using FluentAssertions;
+using Microsoft.AspNetCore.Mvc.Testing;
+using System.Net;
+
+namespace Tutorly.IntegrationTests
+{
+    
+    public class PostControllerTests : IClassFixture<WebApplicationFactory<Program>>
+    {
+        private readonly HttpClient _client;
+
+        public PostControllerTests(WebApplicationFactory<Program> factory)
+        {
+            _client = factory.CreateClient();
+        }
+
+
+        [Fact]
+        public async Task GetPosts_ValidRequest_Returns200()
+        {
+            var response = await _client.GetAsync("api/posts");
+
+            response.StatusCode.Should().Be(HttpStatusCode.OK);
+        }
+
+
+
+    }
+}
