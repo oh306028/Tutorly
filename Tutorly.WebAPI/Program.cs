e383b50cd7c5f8dfb0971d7651f5b507aa9083ee
Oskar <hamerlao@gmail.com>
2024-11-08 17:05:13 +0100

    registered exceptionHandling middleware into the services

--- Tutorly.WebAPI/Program.cs
+++ Tutorly.WebAPI/Program.cs
@@ -1,11 +1,19 @@
 using Microsoft.EntityFrameworkCore;
+using Tutorly.Application.Commands;
+using Tutorly.Application.Handlers;
+using Tutorly.Application.Interfaces;
+using Tutorly.Application.Queries;
+using Tutorly.Application.Services;
+using Tutorly.Domain.Models;
 using Tutorly.Infrastructure;
+using Tutorly.Infrastructure.Repos;
+using Tutorly.WebAPI.Middlewares;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
-builder.Services.AddControllers();
+builder.Services.AddControllers();  
 
 builder.Services.AddDbContext<TutorlyDbContext>(options =>
 {
@@ -13,7 +21,19 @@ builder.Services.AddDbContext<TutorlyDbContext>(options =>
 });
 
 
+builder.Services.AddScoped<IRepository<Post>, PostRepository>();
+builder.Services.AddScoped<IRepository<Tutor>, TutorRepository>();
+builder.Services.AddScoped<IRepository<Category>, CategoryRepository>();
+
+builder.Services.AddScoped<IHandler<CreatePost>, CreatePostHandler>();
+builder.Services.AddScoped<IQueryHandler<GetAllPosts, IEnumerable<Post>>, GetAllPostsHandler>();
+builder.Services.AddScoped<IPostService, PostService>();
+
+builder.Services.AddScoped<ExceptionHandlingMiddleware>();
+
+
 var app = builder.Build();
+app.UseMiddleware<ExceptionHandlingMiddleware>();
 
 // Configure the HTTP request pipeline.
 
@@ -24,3 +44,5 @@ app.UseAuthorization();
 app.MapControllers();
 
 app.Run();
+
+public partial class Program { }
