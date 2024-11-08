2d5bd191e3192f194b4ebb24508e8e45b1b370e5
Oskar <hamerlao@gmail.com>
2024-11-08 17:04:51 +0100

    created the middleware folder; added exceptonHandling middleware; a try catch block into the handler within status codes and messages

---
+++ Tutorly.WebAPI/Middlewares/ExceptionHandlingMiddleware.cs
@@ -0,0 +1,30 @@
+﻿
+using Tutorly.Domain.ModelsExceptions;
+
+namespace Tutorly.WebAPI.Middlewares
+{
+    public class ExceptionHandlingMiddleware : IMiddleware
+    {
+        public async Task InvokeAsync(HttpContext context, RequestDelegate next)
+        {
+            try
+            {
+                await next.Invoke(context);
+
+            }catch(NotFoundException notFound)
+            {
+                context.Response.StatusCode = 404;
+                await context.Response.WriteAsync(notFound.Message);
+            }catch(OutOfSpaceException outOfSpace)
+            {
+                context.Response.StatusCode = 400;
+                await context.Response.WriteAsync(outOfSpace.Message);
+
+            }catch(Exception ex)
+            {
+                context.Response.StatusCode = 500;
+                await context.Response.WriteAsync(ex.Message);
+            }
+        }
+    }
+}
