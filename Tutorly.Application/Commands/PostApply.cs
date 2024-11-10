9afb8f73a95272e29869be34967d375a29d5dde7
Oskar <hamerlao@gmail.com>
2024-11-10 22:57:38 +0100

    added postApply command and handler, prepared for handling the data within the handler

---
+++ Tutorly.Application/Commands/PostApply.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Tutorly.Application.Interfaces;
+using Tutorly.Domain.Models;
+
+namespace Tutorly.Application.Commands
+{
+    public class PostApply : ICommand
+    {
+        public Guid Id { get; set; }
+        public int PostId { get; set; }
+
+    }
+}
---
+++ Tutorly.Application/Handlers/PostApplyHandler.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Tutorly.Application.Commands;
+using Tutorly.Application.Interfaces;
+using Tutorly.Domain.Models;
+using Tutorly.Domain.ModelsExceptions;
+
+namespace Tutorly.Application.Handlers
+{
+    public class PostApplyHandler : IHandler<PostApply>
+    {
+        private readonly IRepository<Post> _postRepository;
+
+        public PostApplyHandler(IRepository<Post> postRepository)
+        {
+            _postRepository = postRepository;
+        }
+
+        //TO DO
+        //user context accesor within the current user id
+        public async Task Handle(PostApply command)
+        {
+            var post =  await _postRepository.GetByIdAsync(command.PostId);
+
+            if (post is null)
+                throw new NotFoundException("Post not found");
+
+
+            // TO DO:
+            //ADD CODE BELOW AFTER STUDENT REPO AND CONTEXT ACCESSOR
+
+            /*
+            var student = 
+
+            post.AddStudent(student);
+            
+            await _postRepository.UpdateAsync();
+            */
+
+        }
+    }
+}
