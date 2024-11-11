1f7b101958d5b0c12f084f65a045619c80894e49
Oskar <hamerlao@gmail.com>
2024-11-11 21:34:07 +0100

    added aunthentication and jwt options into service collection

--- Tutorly.WebAPI/Program.cs
+++ Tutorly.WebAPI/Program.cs
@@ -1,4 +1,9 @@
+using AutoMapper;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.IdentityModel.Tokens;
+using System.Reflection;
+using System.Text;
 using Tutorly.Application.Commands;
 using Tutorly.Application.Handlers;
 using Tutorly.Application.Interfaces;
@@ -7,6 +12,7 @@ using Tutorly.Application.Services;
 using Tutorly.Domain.Models;
 using Tutorly.Infrastructure;
 using Tutorly.Infrastructure.Repos;
+using Tutorly.WebAPI.Authentication;
 using Tutorly.WebAPI.Middlewares;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -29,7 +35,32 @@ builder.Services.AddScoped<IHandler<CreatePost>, CreatePostHandler>();
 builder.Services.AddScoped<IQueryHandler<GetAllPosts, IEnumerable<Post>>, GetAllPostsHandler>();
 builder.Services.AddScoped<IPostService, PostService>();
 
+builder.Services.AddAutoMapper(Assembly.GetExecutingAssembly());
+
+
 builder.Services.AddScoped<ExceptionHandlingMiddleware>();
+
+
+var jwtOptionSection = builder.Configuration.GetRequiredSection("JwtOptions");
+builder.Services.Configure<JwtOptions>(jwtOptionSection);
+
+var jwtOptions = new JwtOptions();
+jwtOptionSection.Bind(jwtOptions);
+builder.Services.AddSingleton(jwtOptions);
+
+
+builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+    .AddJwtBearer(options =>
+    {
+        options.TokenValidationParameters = new TokenValidationParameters
+        {
+            ValidateIssuer = true,
+            ValidateAudience = true,
+            ValidateLifetime = true,
+            ValidAudience = jwtOptionSection["Issuer"],
+            IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(jwtOptionSection["Key"]))
+        };
+    });
 
 
 var app = builder.Build();
@@ -37,8 +68,10 @@ app.UseMiddleware<ExceptionHandlingMiddleware>();
 
 // Configure the HTTP request pipeline.
 
+
 app.UseHttpsRedirection();
 
+app.UseAuthentication();
 app.UseAuthorization();
 
 app.MapControllers();
