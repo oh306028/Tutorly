a76c235eafa138a8334fa8ca5a7fa167046ba4b3
Oskar <hamerlao@gmail.com>
2024-11-11 21:32:27 +0100

    added autoMapping profile, created the map between userRegiste command and dto

---
+++ Tutorly.WebAPI/Mapper.cs
@@ -0,0 +1,19 @@
+﻿using AutoMapper;
+using Tutorly.Application.Commands;
+using Tutorly.Application.Dtos;
+
+namespace Tutorly.WebAPI
+{
+    public class Mapper : Profile
+    {
+        public Mapper()
+        {
+            CreateMap<RegisterUserDto, RegisterUser>()
+                .ForMember(r => r.Role, opt => opt.MapFrom(src => src.Role))
+                .ForMember(g => g.Grade, opt => opt.MapFrom(src => src.Grade));
+
+        }
+
+
+    }
+}
