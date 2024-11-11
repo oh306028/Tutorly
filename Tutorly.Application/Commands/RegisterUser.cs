b0aaa465b93b0db5899b6aa431eec2917d51c6de
Oskar <hamerlao@gmail.com>
2024-11-11 21:33:36 +0100

    created command and handler for handling user registration

---
+++ Tutorly.Application/Commands/RegisterUser.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Tutorly.Application.Interfaces;
+using Tutorly.Domain.Models;
+
+namespace Tutorly.Application.Commands
+{
+    public class RegisterUser : ICommand
+    {
+
+        public RegisterUser(string email, string password, string confirmPassword, string firstName, string lastName, Role role, Grade? grade = null)
+        {
+            Id = Guid.NewGuid();
+            Email = email;
+            Password = password;
+            ConfirmPassword = confirmPassword;
+            FirstName = firstName;
+            LastName = lastName;
+            Role = role;
+            Grade = grade;
+        }
+
+
+        public Guid Id { get; set; }
+        public string Email { get; set; }
+        public string Password { get; set; }
+        public string ConfirmPassword { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public Role Role { get; set; }
+
+        public Grade? Grade { get; set; }
+
+
+    }
+}
---
+++ Tutorly.Application/Handlers/RegisterUserHandler.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Tutorly.Application.Commands;
+using Tutorly.Application.Interfaces;
+using Tutorly.Domain.Models;
+using Tutorly.Domain.ModelsExceptions;
+
+namespace Tutorly.Application.Handlers
+{
+    public class RegisterUserHandler : IHandler<RegisterUser>
+    {
+        private readonly IRepository<User> _userRepository;
+
+        public RegisterUserHandler(IRepository<User> userRepository)
+        {
+            _userRepository = userRepository;   
+        }
+
+
+        public async Task Handle(RegisterUser command)
+        {
+
+            User user;
+
+            if (command.Password != command.ConfirmPassword)
+                throw new ForbidException("Password and confirm password has to be equal!");
+
+            if(command.Role == Role.Tutor)
+            {
+                user = new Tutor()
+                {
+                    FirstName = command.FirstName,
+                    LastName = command.LastName,
+                    Email = command.Email,
+                    Role = Role.Tutor,
+
+                };
+            }
+            else
+            {
+                user = new Student()
+                {
+                    FirstName = command.FirstName,
+                    LastName = command.LastName,
+                    Email = command.Email,
+                    Role = Role.Student,
+                    Grade = (Grade)(command.Grade is null ? Grade.Primary : command.Grade)
+                };
+            }
+
+
+          
+            await _userRepository.AddAsync(user);
+
+        }
+
+    }
+}
