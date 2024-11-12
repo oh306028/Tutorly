8bd84b82e7a639eff64e6e6d96996fce1c65ef33
Oskar <hamerlao@gmail.com>
2024-11-12 23:51:19 +0100

    added forbid and wrongUserInput exceptions|

---
+++ Tutorly.Domain/ModelsExceptions/WrongUserDataInputException.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tutorly.Domain.ModelsExceptions
+{
+    public class WrongUserDataInputException : Exception
+    {
+        public WrongUserDataInputException(string message) : base(message)
+        {
+            
+        }
+    }
+}
