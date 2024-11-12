294c6f88b8b5e218f57549f6ce274ae9bfe70f67
Oskar <hamerlao@gmail.com>
2024-11-12 23:35:56 +0100

    added dto for registering new user

---
+++ Tutorly.Application/Dtos/RegisterUserDto.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Tutorly.Domain.Models;
+
+namespace Tutorly.Application.Dtos
+{
+    public class RegisterUserDto
+    {
+        public string Email { get; set; }
+        public string Password { get; set; }
+        public string ConfirmPassword { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Role { get; set; }
+
+        public string? Grade { get; set; } = null;
+    }
+}
