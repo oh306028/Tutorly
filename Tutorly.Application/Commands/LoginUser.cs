f661fbbc1f4a83105fdeb1702aa48d480236970b
Oskar <hamerlao@gmail.com>
2024-11-13 23:24:15 +0100

    created dto and command for login user

---
+++ Tutorly.Application/Commands/LoginUser.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Tutorly.Application.Interfaces;
+
+namespace Tutorly.Application.Commands
+{
+    public class LoginUser : ICommand
+    {
+        public Guid Id { get; init; }
+        public string Email { get; set; }
+        public string Password { get; set; }    
+
+        public LoginUser(string email, string password)
+        {
+            Id = Guid.NewGuid();
+            Email = email;
+            Password = password;
+        }
+    }
+}
