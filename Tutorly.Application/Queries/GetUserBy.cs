3295cb18bcfc3f75094779781c51cf3047452378
Oskar <hamerlao@gmail.com>
2024-11-13 23:25:14 +0100

    added query for getting user by predicate

---
+++ Tutorly.Application/Queries/GetUserBy.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+using Tutorly.Application.Interfaces;
+using Tutorly.Domain.Models;
+
+namespace Tutorly.Application.Queries
+{
+    public class GetUserBy : IQuery<User>
+    {
+        public Expression<Func<User, bool>> Predicate { get; set; }
+
+        public GetUserBy(Expression<Func<User, bool>> predicate)
+        {
+            Predicate = predicate;
+        }
+    }
+}
