f8d76b07c631d22a1bc5ea028a56636293aefa61
Oskar <hamerlao@gmail.com>
2024-11-13 23:24:55 +0100

    added hanlders for logging command and getUserBy query

---
+++ Tutorly.Application/Handlers/GetUserByHandler.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+using Tutorly.Application.Interfaces;
+using Tutorly.Application.Queries;
+using Tutorly.Domain.Models;
+
+namespace Tutorly.Application.Handlers
+{
+    public class GetUserByHandler : IQueryHandler<GetUserBy, User>
+    {
+        private readonly IRepository<User> _userRepository;
+
+        public GetUserByHandler(IRepository<User> userRepository)
+        {
+            _userRepository = userRepository;   
+        }
+        public async Task<User> HandleAsync(GetUserBy query)
+        {
+            var user = await _userRepository.GetBy(query.Predicate);
+
+            return user;
+
+        }
+
+
+    }
+}
---
+++ Tutorly.Application/Handlers/LoginUserHandler.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.AspNet.Identity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Tutorly.Application.Commands;
+using Tutorly.Application.Interfaces;
+using Tutorly.Domain.Models;
+using Tutorly.Domain.ModelsExceptions;
+
+namespace Tutorly.Application.Handlers
+{
+    public class LoginUserHandler : IHandler<LoginUser>
+    {
+        private readonly IRepository<User> _userRepository;
+
+        public LoginUserHandler(IRepository<User> userRepository)
+        {
+            _userRepository = userRepository;
+        }
+        public async Task Handle(LoginUser command)
+        {
+            var userToLogIn = await _userRepository.GetBy(e => e.Email == command.Email);
+
+            if (userToLogIn is null)
+                throw new NotFoundException("Incorrect email or password");
+
+            var hasher = new PasswordHasher();
+            var result = hasher.VerifyHashedPassword(userToLogIn.PasswordHash, command.Password);
+
+            if (result == PasswordVerificationResult.Failed)
+                throw new NotFoundException("Incorrect email or password");
+
+        }
+    }
+}
