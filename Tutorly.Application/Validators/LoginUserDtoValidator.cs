716031f2029039f016e3b6597998ecc00462bf5e
Oskar <hamerlao@gmail.com>
2024-11-14 15:28:38 +0100

    created registerDto and loginDto validators using fluentValidations

---
+++ Tutorly.Application/Validators/LoginUserDtoValidator.cs
@@ -0,0 +1,19 @@
+﻿using FluentValidation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Tutorly.Application.Dtos;
+
+namespace Tutorly.Application.Validators
+{
+    public class LoginUserDtoValidator : AbstractValidator<LoginUserDto>
+    {
+        public LoginUserDtoValidator()
+        {
+            RuleFor(e => e.Email).NotEmpty();
+            RuleFor(p => p.Password).NotEmpty();
+        }
+    }
+}
---
+++ Tutorly.Application/Validators/RegisterUserDtoValidator.cs
@@ -0,0 +1,53 @@
+﻿using FluentValidation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Tutorly.Application.Dtos;
+using Tutorly.Application.Interfaces;
+using Tutorly.Domain.Models;
+
+namespace Tutorly.Application.Validators
+{
+    public class RegisterUserDtoValidator : AbstractValidator<RegisterUserDto>
+    {   
+        public RegisterUserDtoValidator(IRepository<User> userRepository)
+        {      
+
+            RuleFor(e => e.Email)
+                .NotEmpty()
+                .EmailAddress()
+                .MaximumLength(30);
+
+            RuleFor(p => p.Password)
+                .NotEmpty()
+                .MinimumLength(3);
+
+            RuleFor(cp => cp.ConfirmPassword)
+                .NotEmpty()
+                .Equal(p => p.Password);
+
+            RuleFor(fn => fn.FirstName)
+                .NotEmpty()
+                .MaximumLength(15);
+
+            RuleFor(ln => ln.LastName)
+                .NotEmpty()
+                .MaximumLength(20);
+
+            RuleFor(r => r.Role)
+                .NotEmpty();
+
+            RuleFor(e => e.Email).Custom((email, context) => {
+                var emailInUse = userRepository.GetBy(e => e.Email == email);
+
+                if(!(emailInUse is null))
+                {
+                    context.AddFailure("Email is already in use");
+                }
+            }); 
+            
+        }
+    }
+}
