8db9fa81e2aa47d56095a32d24b4a83f73811e67
Oskar <hamerlao@gmail.com>
2024-11-15 21:44:18 +0100

    added a helper service => userContext within the current request claims

---
+++ Tutorly.Application/Services/UserContextService.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Http;
+using System.Security.Claims;
+using Tutorly.Domain.Models;
+
+namespace Tutorly.WebAPI.Services
+{
+    public interface IUserContextService
+    {
+        int? GetUserId { get; }
+        ClaimsPrincipal User { get; }
+    }
+
+    public class UserContextService : IUserContextService
+    {
+        private readonly IHttpContextAccessor _context;
+
+        public UserContextService(IHttpContextAccessor context)
+        {
+            _context = context;
+        }
+        public int? GetUserId => User is null ? null : int.Parse(User.FindFirst(c => c.Type == ClaimTypes.NameIdentifier).Value);
+
+        public ClaimsPrincipal User => _context.HttpContext?.User;
+    }
+}
