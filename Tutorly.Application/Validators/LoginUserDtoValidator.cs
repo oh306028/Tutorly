3d9c5295f370f47782fd1b43e43237f1db734bf5
Oskar <hamerlao@gmail.com>
2024-11-16 23:23:36 +0100

    added rules for validator of loggin and register user

--- Tutorly.Application/Validators/LoginUserDtoValidator.cs
+++ Tutorly.Application/Validators/LoginUserDtoValidator.cs
@@ -4,7 +4,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using Tutorly.Application.Dtos;
+using Tutorly.Application.Dtos.CreateDtos;
 
 namespace Tutorly.Application.Validators
 {
