45a8c96a863a3fc125935d8b572964ab2a2cd021
Oskar <hamerlao@gmail.com>
2024-11-19 18:15:04 +0100

    added more mappings between dtos

--- Tutorly.WebAPI/Mapper.cs
+++ Tutorly.WebAPI/Mapper.cs
@@ -1,6 +1,8 @@
 ﻿using AutoMapper;
 using Tutorly.Application.Commands;
-using Tutorly.Application.Dtos;
+using Tutorly.Application.Dtos.CreateDtos;
+using Tutorly.Application.Dtos.DisplayDtos;
+using Tutorly.Domain.Models;
 
 namespace Tutorly.WebAPI
 {
@@ -13,6 +15,16 @@ namespace Tutorly.WebAPI
                 .ForMember(g => g.Grade, opt => opt.MapFrom(src => src.Grade));
 
             CreateMap<LoginUserDto, LoginUser>();
+
+            CreateMap<Post, PostWithTutorDto>()
+              .ForMember(dest => dest.CurrentStudentAmount, opt => opt.MapFrom(src => src.CurrentStudentAmount))
+              .ForMember(dest => dest.Students, opt => opt.MapFrom(src => src.Students))
+              .ForMember(dest => dest.IsAtStudentPlace, opt => opt.MapFrom(src => src.IsAtStudentPlace));
+
+          
+            CreateMap<Tutor, TutorDto>();
+            CreateMap<Address, AddressDto>();
+            CreateMap<Category, CategoryDto>();
 
         }
 
