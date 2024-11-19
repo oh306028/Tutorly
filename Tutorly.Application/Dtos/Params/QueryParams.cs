9c4265d4bc956b543731e8f3d9fdce61462550ca
Oskar <hamerlao@gmail.com>
2024-11-19 18:15:32 +0100

    added mistakly deleted params

---
+++ Tutorly.Application/Dtos/Params/QueryParams.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tutorly.Application.Dtos.Params
+{
+    public class QueryParams
+    {
+        public int? CategoryId { get; set; }
+    }
+}
