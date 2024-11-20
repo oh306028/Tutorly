4e34e3eda28d31740358f237e6bdeb1209ba84b1
Oskar <hamerlao@gmail.com>
2024-11-20 21:44:59 +0100

    created tutor integration tests for restoring all tutors

---
+++ Tutorly.IntegrationTests/TutorControllerTest.cs
@@ -0,0 +1,25 @@
+﻿using FluentAssertions;
+using Microsoft.AspNetCore.Mvc.Testing;
+using System.Net;
+using Tutorly.Application.Dtos.Params;
+
+namespace Tutorly.IntegrationTests
+{
+    public class TutorControllerTest : IClassFixture<WebApplicationFactory<Program>>
+    {
+        private readonly HttpClient _client;
+
+        public TutorControllerTest(WebApplicationFactory<Program> factory)
+        {
+             _client = factory.CreateClient();
+        }
+
+        [Fact]
+        public async Task GetAllTutors_ReturnsOK()
+        {
+            var response = await _client.GetAsync("api/tutors");
+                
+            response.StatusCode.Should().Be(HttpStatusCode.OK);
+        }
+    }
+}
