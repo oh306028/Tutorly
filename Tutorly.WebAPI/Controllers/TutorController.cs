911b8b2df7df7f136e8b4b6722060558704d39c8
Oskar <hamerlao@gmail.com>
2024-11-20 21:44:32 +0100

    added tutor service and controller; HttpGet method initialzied

---
+++ Tutorly.WebAPI/Controllers/TutorController.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Tutorly.Application.Dtos.DisplayDtos;
+using Tutorly.Application.Dtos.Params;
+using Tutorly.WebAPI.Services;
+
+namespace Tutorly.WebAPI.Controllers
+{
+    [Route("api/tutors")]
+    [ApiController]
+    public class TutorController : ControllerBase
+    {
+        private readonly ITutorService _tutorService;
+
+        public TutorController(ITutorService tutorService)
+        {
+            _tutorService = tutorService;
+        }
+
+
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<TutorWithPostsDto>>> GetAllTutors([FromQuery] QueryParams queryParams)
+        {
+            var result = await _tutorService.GetAllTutors(queryParams);
+
+            return Ok(result);
+        }
+
+
+    }
+}
---
+++ Tutorly.WebAPI/Services/TutorService.cs
@@ -0,0 +1,40 @@
+﻿using AutoMapper;
+using Tutorly.Application.Dtos.DisplayDtos;
+using Tutorly.Application.Dtos.Params;
+using Tutorly.Application.Interfaces;
+using Tutorly.Application.Queries;
+using Tutorly.Domain.Models;
+
+namespace Tutorly.WebAPI.Services
+{
+    public interface ITutorService
+    {
+        Task<IEnumerable<TutorWithPostsDto>> GetAllTutors(QueryParams queryParams);
+    }
+
+    public class TutorService : ITutorService
+    {
+        private readonly IQueryHandler<GetAllTutors, IEnumerable<Tutor>> _getAllTutorsHandler;
+        private readonly IMapper _mapper;
+
+        public TutorService(IQueryHandler<GetAllTutors, IEnumerable<Tutor>> getAllTutorsHandler, IMapper mapper)
+        {
+            _getAllTutorsHandler = getAllTutorsHandler;
+            _mapper = mapper;
+        }
+
+        public async Task<IEnumerable<TutorWithPostsDto>> GetAllTutors(QueryParams queryParams)
+        {
+            var query = new GetAllTutors(queryParams);
+            var results = await _getAllTutorsHandler.HandleAsync(query);
+
+            var mappedResults = _mapper.Map<IEnumerable<TutorWithPostsDto>>(results);
+            return mappedResults;
+
+        }
+
+
+
+
+    }
+}
