f9bdd7d29d9bb5910ef0a95d75806da8fdd7c7b8
Oskar <hamerlao@gmail.com>
2024-11-21 21:49:51 +0100

    refactored the code; added the param

--- Tutorly.Application/Queries/GetAllPosts.cs
+++ Tutorly.Application/Queries/GetAllPosts.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using Tutorly.Application.Dtos;
+using Tutorly.Application.Dtos.Params;
 using Tutorly.Application.Interfaces;
 using Tutorly.Domain.Models;
 
