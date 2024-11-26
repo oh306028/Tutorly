0efc6dfecc3f1901cdbb1802190f64ea224bd0a2
Oskar <hamerlao@gmail.com>
2024-11-26 22:44:38 +0100

    created the categoryController and its service

---
+++ Tutorly.WebAPI/Controllers/CategoryController.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Tutorly.Application.Dtos.DisplayDtos;
+using Tutorly.WebAPI.Services;
+
+namespace Tutorly.WebAPI.Controllers
+{
+    [Route("api/categories")]
+    [ApiController]
+    public class CategoryController : ControllerBase
+    {
+        private readonly ICategoryService _categoryService;
+
+        public CategoryController(ICategoryService categoryService)
+        {
+            _categoryService = categoryService;
+        }
+
+
+
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<CategoryDto>>> GetAllCategories()
+        {
+            var result = await _categoryService.GetAllCategories();
+            return Ok(result);
+        }
+
+
+
+    }
+}
---
+++ Tutorly.WebAPI/Services/CategoryService.cs
@@ -0,0 +1,40 @@
+﻿using AutoMapper;
+using Tutorly.Application.Dtos.DisplayDtos;
+using Tutorly.Application.Interfaces;
+using Tutorly.Application.Queries;
+using Tutorly.Domain.Models;
+
+namespace Tutorly.WebAPI.Services
+{
+    public interface ICategoryService
+    {
+        Task<IEnumerable<CategoryDto>> GetAllCategories();
+    }
+
+        
+    public class CategoryService : ICategoryService
+    {
+        private readonly IQueryHandler<GetAllCategories, IEnumerable<Category>> _getCategoriesHandler;
+        private readonly IMapper _mapper;
+
+        public CategoryService(IQueryHandler<GetAllCategories, IEnumerable<Category>> getCategoriesHandler, IMapper mapper)
+        {
+            _getCategoriesHandler = getCategoriesHandler;
+            _mapper = mapper;
+        }
+
+
+        public async Task<IEnumerable<CategoryDto>> GetAllCategories()
+        {
+            var query = new GetAllCategories();
+            var categories = await _getCategoriesHandler.HandleAsync(query);
+
+            var result = _mapper.Map<IEnumerable<CategoryDto>>(categories);
+            return result;
+
+        }
+
+
+
+    }
+}
