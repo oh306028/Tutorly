72c659afa63d5c7454af194caa90be2d8509793f
Oskar <hamerlao@gmail.com>
2024-11-26 22:45:31 +0100

    created tests for retreiving all categories

---
+++ Tutorly.IntegrationTests/CategoryControllerTests.cs
@@ -0,0 +1,25 @@
+﻿using FluentAssertions;
+using Microsoft.AspNetCore.Mvc.Testing;
+using System.Net;
+
+namespace Tutorly.IntegrationTests
+{
+    public class CategoryControllerTests : IClassFixture<WebApplicationFactory<Program>>
+    {
+        private readonly HttpClient client;
+
+        public CategoryControllerTests(WebApplicationFactory<Program> factory)
+        {
+            client = factory.CreateClient();
+        }
+
+
+        [Fact]
+        public async Task GetCategories_ReturnsOk()
+        {
+            var response = await client.GetAsync("api/categories");
+
+            response.StatusCode.Should().Be(HttpStatusCode.OK);
+        }
+    }
+}
