d99c41dcf8a694c33127dc78497291be64d3c33b
Oskar <hamerlao@gmail.com>
2024-11-26 22:44:08 +0100

    added get all categories handler and query

---
+++ Tutorly.Application/Handlers/GetAllCategoriesHandler.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Tutorly.Application.Interfaces;
+using Tutorly.Application.Queries;
+using Tutorly.Domain.Models;
+
+namespace Tutorly.Application.Handlers
+{
+    public class GetAllCategoriesHandler : IQueryHandler<GetAllCategories, IEnumerable<Category>>
+    {
+        private readonly IRepository<Category> _categoryRepository;
+
+        public GetAllCategoriesHandler(IRepository<Category> categoryRepository)
+        {
+            _categoryRepository = categoryRepository;
+        }
+
+
+        public async Task<IEnumerable<Category>> HandleAsync(GetAllCategories query)
+        {
+            var categories = await _categoryRepository.GetAllAsync();
+            return categories;
+        }
+
+
+    }
+}
---
+++ Tutorly.Application/Queries/GetAllCategories.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Tutorly.Application.Interfaces;
+using Tutorly.Domain.Models;
+
+namespace Tutorly.Application.Queries
+{
+    public class GetAllCategories : IQuery<IEnumerable<Category>>
+    {
+
+    }
+}
