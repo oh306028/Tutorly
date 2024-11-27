fc164316da1430794cf825fe924e7d296a535750
Oskar <hamerlao@gmail.com>
2024-11-27 16:50:01 +0100

    changed the property modifiers to set and get instead of get only

--- Tutorly.Application/Dtos/CreateDtos/CreatePostDto.cs
+++ Tutorly.Application/Dtos/CreateDtos/CreatePostDto.cs
@@ -9,15 +9,15 @@ namespace Tutorly.Application.Dtos.CreateDtos
 {
     public class CreatePostDto
     {
-        public int CategoryId { get; }
-        public int TutorId { get; }
-        public int MaxStudentAmount { get; }
-        public DayOfWeek HappensOn { get; }
-        public TimeSpan HappensAt { get; }
-        public bool IsRemotely { get; }
-        public bool IsAtStudentPlace { get; }
-        public string Description { get; }
-        public int? AddressId { get; }
-        public Grade StudentsGrade { get; }
+        public int CategoryId { get; set; }
+        public int TutorId { get; set; }
+        public int MaxStudentAmount { get; set; }
+        public DayOfWeek HappensOn { get; set; }
+        public TimeSpan HappensAt { get; set; }
+        public bool IsRemotely { get; set; }
+        public bool IsAtStudentPlace { get; set; }
+        public string Description { get; set; }
+        public int? AddressId { get; set; }
+        public Grade StudentsGrade { get; set; }
     }
 }
