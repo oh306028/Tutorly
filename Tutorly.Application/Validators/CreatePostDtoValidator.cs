053f8cf1b49af13c11915e607844635fb05abc06
Oskar <hamerlao@gmail.com>
2024-11-27 16:50:44 +0100

    added validator for createPost dto

---
+++ Tutorly.Application/Validators/CreatePostDtoValidator.cs
@@ -0,0 +1,38 @@
+﻿using FluentValidation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Tutorly.Application.Dtos.CreateDtos;
+
+namespace Tutorly.Application.Validators
+{
+    public class CreatePostDtoValidator : AbstractValidator<CreatePostDto>
+    {
+        public CreatePostDtoValidator()
+        {
+            RuleFor(c => c.CategoryId)
+                .NotEmpty();
+
+            RuleFor(s => s.MaxStudentAmount)
+                .NotEmpty()
+                .GreaterThan(0)
+                .LessThan(20);
+
+            RuleFor(r => r.IsRemotely)
+                .NotNull();
+
+            RuleFor(p => p.IsAtStudentPlace)
+                .NotNull();
+
+            
+            RuleFor(g => g.StudentsGrade)
+                .NotEmpty();
+
+            RuleFor(t => t.HappensAt)
+                .NotEmpty();
+
+        }
+    }
+}
