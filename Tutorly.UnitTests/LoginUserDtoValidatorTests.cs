8a10e8ed8a294c71ace5b34ec1ea8ba144f9189a
Oskar <hamerlao@gmail.com>
2024-11-28 21:55:45 +0100

    created loginUserValidator tests

---
+++ Tutorly.UnitTests/LoginUserDtoValidatorTests.cs
@@ -0,0 +1,43 @@
+﻿using FluentValidation.TestHelper;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Text;
+using System.Threading.Tasks;
+using Tutorly.Application.Dtos.CreateDtos;
+using Tutorly.Application.Validators;
+
+namespace Tutorly.UnitTests
+{
+    public class LoginUserDtoValidatorTests
+    {
+        public static IEnumerable<object[]> GetValidLoginExampleData()
+        {
+            var list = new List<LoginUserDto>() { 
+            
+                new LoginUserDto()
+                {
+                    Email = "test@mail",
+                    Password = "password1"
+                }
+            
+            };
+
+            return list.Select(s => new object[] { s });
+        }
+
+
+        [Theory]
+        [MemberData(nameof(GetValidLoginExampleData))]
+        public void LoginUserDto_ValidData_NoErrors(LoginUserDto model)
+        {
+            var validator = new LoginUserDtoValidator();
+            var response = validator.TestValidate(model);
+
+            response.ShouldNotHaveAnyValidationErrors();
+
+        }
+        
+    }
+}
