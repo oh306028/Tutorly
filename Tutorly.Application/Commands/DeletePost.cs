6852d54e4f89b79cf214565fd5fc1e04d8d4d1ec
Oskar <hamerlao@gmail.com>
2024-11-29 23:00:50 +0100

    created the handler and command for deleting a post by tutor

---
+++ Tutorly.Application/Commands/DeletePost.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Tutorly.Application.Interfaces;
+
+namespace Tutorly.Application.Commands
+{
+    public class DeletePost : ICommand
+    {
+        public Guid Id { get; set; }
+        public int PostId { get; set; } 
+
+        public DeletePost(int postId)
+        {
+            Id = Guid.NewGuid();
+            PostId = postId;
+        }
+    }
+}
---
+++ Tutorly.Application/Handlers/DeletePostHandler.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Tutorly.Application.Commands;
+using Tutorly.Application.Interfaces;
+using Tutorly.Domain.Models;
+using Tutorly.Domain.ModelsExceptions;
+
+namespace Tutorly.Application.Handlers
+{
+    public class DeletePostHandler : IHandler<DeletePost>
+    {
+        private readonly IRepository<Post> _postRepository;
+
+        public DeletePostHandler(IRepository<Post> postRepository)
+        {
+            _postRepository = postRepository;
+        }
+
+        public async Task Handle(DeletePost command)
+        {
+            var postToDelete = await _postRepository.GetByIdAsync(command.PostId);
+
+            if (postToDelete is null)
+                throw new NotFoundException("Post not found");
+
+            await _postRepository.DeleteAsync(postToDelete);
+
+        }
+
+
+    }
+}
