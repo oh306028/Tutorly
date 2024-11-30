1d29201b87649f46737504eac3fd8d1e66abee48
Oskar <hamerlao@gmail.com>
2024-11-30 17:35:42 +0100

    created query for getting user data; added handler for retreiving data form db

---
+++ Tutorly.Application/Handlers/GetUserDataHandler.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Tutorly.Application.Interfaces;
+using Tutorly.Application.Queries;
+using Tutorly.Domain.Models;
+using Tutorly.Domain.ModelsExceptions;
+
+namespace Tutorly.Application.Handlers
+{
+    public class GetUserDataHandler : IQueryHandler<GetUserData, User>
+    {
+        private readonly IRepository<User> _userRepository;
+
+        public GetUserDataHandler(IRepository<User> userRepository)
+        {
+            _userRepository = userRepository;
+
+        }
+
+
+        public async Task<User> HandleAsync(GetUserData query)
+        {
+           var userData = await _userRepository.GetByIdAsync(query.UserId);
+
+            if (userData is null)
+                throw new NotFoundException("User not found");
+
+            return userData;
+        }
+    }
+}
---
+++ Tutorly.Application/Queries/GetUserData.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Tutorly.Application.Interfaces;
+using Tutorly.Domain.Models;
+
+namespace Tutorly.Application.Queries
+{
+    public class GetUserData : IQuery<User>
+    {
+        public GetUserData(int userId)
+        {
+            UserId = userId;
+        }
+        public int UserId { get; set; } 
+    }
+}
