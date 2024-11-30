14abb7986c82f84ab420897f30c3e2a14efecf41
Oskar <hamerlao@gmail.com>
2024-11-30 17:34:56 +0100

    created endpoint for retreiving user data; needed for personal user data pannel; added method for service as well

--- Tutorly.WebAPI/Controllers/UserController.cs
+++ Tutorly.WebAPI/Controllers/UserController.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
-using Tutorly.Application.Dtos;
+using Tutorly.Application.Dtos.CreateDtos;
+using Tutorly.Application.Dtos.DisplayDtos;
 using Tutorly.WebAPI.Services;
 
 namespace Tutorly.WebAPI.Controllers
@@ -18,14 +19,38 @@ namespace Tutorly.WebAPI.Controllers
 
 
         [HttpPost("register")]
-        public async Task<ActionResult> RegisterUser(RegisterUserDto dto)
+        public async Task<ActionResult> RegisterUser([FromBody] RegisterUserDto dto)
         {
             await _userService.RegisterUserAsync(dto);
 
             return Created("/api/accounts", null);
         }
 
+        [HttpPost("login")]
+        public async Task<ActionResult<string>> LoginUser([FromBody] LoginUserDto dto)
+        {
+            var result = await _userService.LoginUser(dto);
 
+            return Ok(result);
+        }
+
+        [HttpDelete("userId")]
+        public async Task<ActionResult> DeleteUser([FromRoute] int userId)
+        {
+            await _userService.DeleteUser(userId);
+
+            return Ok();  
+        
+        }
+
+
+        [HttpGet("{userId}")]
+        public async Task<ActionResult<UserDataDto>> GetUserData([FromRoute] int userId)
+        {
+            var result = await _userService.GetUserData(userId);
+
+            return Ok(result);
+        }
 
 
     }
--- Tutorly.WebAPI/Services/UserService.cs
+++ Tutorly.WebAPI/Services/UserService.cs
@@ -5,8 +5,11 @@ using System.IdentityModel.Tokens.Jwt;
 using System.Security.Claims;
 using System.Text;
 using Tutorly.Application.Commands;
-using Tutorly.Application.Dtos;
+using Tutorly.Application.Dtos.CreateDtos;
+using Tutorly.Application.Dtos.DisplayDtos;
 using Tutorly.Application.Handlers;
+using Tutorly.Application.Interfaces;
+using Tutorly.Application.Queries;
 using Tutorly.Domain.Models;
 using Tutorly.WebAPI.Authentication;
 
@@ -15,19 +18,45 @@ namespace Tutorly.WebAPI.Services
     public interface IUserService
     {
         Task RegisterUserAsync(RegisterUserDto dto);
+        Task DeleteUser(int userId);
+
+        Task<string> LoginUser(LoginUserDto dto);
+        Task<UserDataDto> GetUserData(int userId);
     }
 
     public class UserService : IUserService
     {
         private readonly JwtOptions _jwtOptions;
         private readonly IHandler<RegisterUser> _registerUserHandler;
-        private readonly IMapper _mapper;
 
-        public UserService(JwtOptions jwtOptions, IHandler<RegisterUser> registerUserHandler, IMapper mapper)
+        private readonly IMapper _mapper;
+        private readonly IHandler<DeleteUser> _deleteUserHandler;
+        private readonly IHandler<LoginUser> _loginUserHandler;
+        private readonly IQueryHandler<GetUserBy, User> _getUserByHandler;
+        private readonly IQueryHandler<GetUserData, User> _getUserDataHandler;
+
+        public UserService(JwtOptions jwtOptions, IHandler<RegisterUser> registerUserHandler,
+            IMapper mapper, IHandler<DeleteUser> deleteUserHandler, IHandler<LoginUser> loginUserHandler,
+            IQueryHandler<GetUserBy, User> getUserByHandler, IQueryHandler<GetUserData, User> getUserDataHandler)    
         {
             _jwtOptions = jwtOptions;
             _registerUserHandler = registerUserHandler;
             _mapper = mapper;
+            _deleteUserHandler = deleteUserHandler;
+            _loginUserHandler = loginUserHandler;
+            _getUserByHandler = getUserByHandler;
+            _getUserDataHandler = getUserDataHandler;
+        }
+
+
+        public async Task<UserDataDto> GetUserData(int userId)
+        {
+            var query = new GetUserData(userId);
+            var userData = await _getUserDataHandler.HandleAsync(query);
+
+            var mappedResult = _mapper.Map<UserDataDto>(userData);
+            return mappedResult;
+
         }
 
         public async Task RegisterUserAsync(RegisterUserDto dto)    
@@ -37,8 +66,24 @@ namespace Tutorly.WebAPI.Services
             await _registerUserHandler.Handle(command);
         }
 
+        public async Task DeleteUser(int userId)
+        {
+            var command = new DeleteUser(userId);
+            await _deleteUserHandler.Handle(command);
+        }
 
+        public async Task<string> LoginUser(LoginUserDto dto)
+        {
+            var command = _mapper.Map<LoginUser>(dto);
+            await _loginUserHandler.Handle(command);
 
+            var query = new GetUserBy(x => x.Email == dto.Email);   
+            var user = await _getUserByHandler.HandleAsync(query);
+
+            var token = GenerateToken(user);
+            return token;
+
+        }
 
         private string GenerateToken(User user)
         {
@@ -49,11 +94,10 @@ namespace Tutorly.WebAPI.Services
                 Subject = new ClaimsIdentity(new[]
                 {
                 new Claim(ClaimTypes.Name, user.Email),
-                new Claim(ClaimTypes.NameIdentifier, user.Id.ToString()),
-                new Claim(ClaimTypes.Role, user.Role.ToString())
-
+                new Claim(ClaimTypes.Role, Enum.GetName(typeof(Role), user.Role) ?? string.Empty)
+               
             }),
-                Expires = DateTime.UtcNow.AddMinutes(120),
+                Expires = DateTime.UtcNow.AddHours(1),
                 Issuer = _jwtOptions.Issuer,
                 SigningCredentials = new SigningCredentials(securityKey, SecurityAlgorithms.HmacSha256Signature)
             };
