fd6548cda886d40286eb5e8508c1473675c7550f
Oskar <hamerlao@gmail.com>
2024-12-01 18:56:38 +0100

    added handler and command for updating user data

---
+++ Tutorly.Application/Commands/UpdateUserData.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Tutorly.Application.Interfaces;
+
+namespace Tutorly.Application.Commands
+{
+    public class UpdateUserData : ICommand
+    {
+
+        public UpdateUserData(int userId, string firstName = null, string lastName = null, string email = null, byte? experience = null, string grade = null, string description = null)
+        {
+            Id = Guid.NewGuid();
+            UserId = userId;
+            FirstName = firstName;
+            LastName = lastName;
+            Email = email;
+            Experience = experience;
+            Grade = grade;
+            Description = description;
+        }
+
+        public int UserId { get; }
+        public string? FirstName { get; set; }
+        public string? LastName { get; set; }
+        public string? Email { get; set; }
+
+
+        //Tutor
+        public byte? Experience { get; set; }
+        public string? Description { get; set; }
+
+        //StudentOnly
+        public string? Grade { get; set; }
+
+        public Guid Id { get; }
+    }
+}
---
+++ Tutorly.Application/Handlers/UpdateUserDataHandler.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Tutorly.Application.Commands;
+using Tutorly.Application.Interfaces;
+using Tutorly.Domain.Models;
+using Tutorly.Domain.ModelsExceptions;
+
+namespace Tutorly.Application.Handlers
+{
+    public class UpdateUserDataHandler : IHandler<UpdateUserData>
+    {
+        private readonly IRepository<User> _userRepository;
+
+        public UpdateUserDataHandler(IRepository<User> userRepository)
+        {
+            _userRepository = userRepository;
+        }
+
+
+        public async Task Handle(UpdateUserData command)
+        {
+            var userToUpdate = await _userRepository.GetByIdAsync(command.UserId);
+
+            if (userToUpdate is null)
+                throw new NotFoundException("User not found");
+
+            userToUpdate.FirstName = command.FirstName ?? userToUpdate.FirstName;
+            userToUpdate.LastName = command.LastName ?? userToUpdate.LastName;
+            userToUpdate.Email = command.Email ?? userToUpdate.Email;
+
+
+            if(userToUpdate is Tutor tutor)
+            {
+                tutor.Description = command.Description ?? tutor.Description;
+                tutor.Experience = command.Experience ?? tutor.Experience;
+
+            }else if(userToUpdate is Student student && command.Grade != null)
+            {
+                
+                var gradeToChange = Enum.Parse<Grade>(command.Grade);
+                student.Grade = gradeToChange;
+                
+            }
+
+            await _userRepository.UpdateAsync(userToUpdate);
+            
+
+
+        }
+
+
+    }
+}
