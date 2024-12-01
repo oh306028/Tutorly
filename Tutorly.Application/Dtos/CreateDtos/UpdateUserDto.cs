903a5e5f5c1be33c913260453573b5cf8411f3ae
Oskar <hamerlao@gmail.com>
2024-12-01 18:58:08 +0100

    updateUser dto and UserData dto; for displaying and updating the data dtos

---
+++ Tutorly.Application/Dtos/CreateDtos/UpdateUserDto.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tutorly.Application.Dtos.CreateDtos
+{
+    public class UpdateUserDto
+    {
+        public int UserId { get; }
+        public string? FirstName { get; set; }
+        public string? LastName { get; set; }
+        public string? Email { get; set; }
+        public byte? Experience { get; set; }
+        public string? Description { get; set; }
+        public string? Grade { get; set; }
+
+    }
+}
---
+++ Tutorly.Application/Dtos/DisplayDtos/UserDataDto.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Tutorly.Domain.Models;
+
+namespace Tutorly.Application.Dtos.DisplayDtos
+{
+    public class UserDataDto
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Email { get; set; }
+
+
+        public string Role { get; set; }
+
+        //TutorOnly
+        public byte? Experience { get; set; }
+        public string? Description { get; set; }
+
+        //StudentOnly
+        public string? Grade { get; set; }
+
+    }
+}
