7bc499c06bb9ade8f34e79f62e0253d21a25471f
Oskar <hamerlao@gmail.com>
2024-12-08 19:19:09 +0100

    deleting user command and handler added

---
+++ Tutorly.Application/Commands/DeleteUser.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Tutorly.Application.Interfaces;
+
+namespace Tutorly.Application.Commands
+{
+    public class DeleteUser : ICommand
+    {
+        public Guid Id { get; init; }
+        public int UserId { get; set; } 
+
+        public DeleteUser(int userId)   
+        {
+            Id = Guid.NewGuid();
+            UserId = userId;
+        }
+    }
+}
---
+++ Tutorly.Application/Handlers/DeleteUserHandler.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Tutorly.Application.Commands;
+using Tutorly.Application.Interfaces;
+using Tutorly.Domain.Models;
+using Tutorly.Domain.ModelsExceptions;
+
+namespace Tutorly.Application.Handlers
+{
+    public class DeleteUserHandler : IHandler<DeleteUser>
+    {
+        private readonly IRepository<User> _userRepository;
+
+        public DeleteUserHandler(IRepository<User> userRepository)
+        {
+            _userRepository = userRepository;
+        }
+        public async Task Handle(DeleteUser command)
+        {
+            var userToDelete = await _userRepository.GetByIdAsync(command.UserId);
+
+            if (userToDelete is null)
+                throw new NotFoundException("User to delete not found");
+
+            await _userRepository.DeleteAsync(userToDelete);
+        }
+    }
+}
