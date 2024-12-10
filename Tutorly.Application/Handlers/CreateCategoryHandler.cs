028fb1e994d37f39316e536c12c60ef67c6d5344
Oskar <hamerlao@gmail.com>
2024-12-10 23:09:04 +0100

    created handler for creating new category

---
+++ Tutorly.Application/Handlers/CreateCategoryHandler.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Tutorly.Application.Commands;
+using Tutorly.Application.Interfaces;
+using Tutorly.Domain.Models;
+
+namespace Tutorly.Application.Handlers
+{
+    public class CreateCategoryHandler : IHandler<CreateCategory>
+    {
+        private readonly IRepository<Category> _categoryRepository;
+
+        public CreateCategoryHandler(IRepository<Category> categoryRepository)
+        {
+            _categoryRepository = categoryRepository;
+        }
+
+        public async Task Handle(CreateCategory command)
+        {
+
+            var category = new Category()
+            {
+                Name = command.Name
+
+            };
+
+            await _categoryRepository.AddAsync(category);
+
+
+        }
+    }
+}
