f20d04bc0211ef5eedc58a3e7407460f97431f27
Oskar <hamerlao@gmail.com>
2024-12-10 23:07:59 +0100

    created command and dto for creating new category

---
+++ Tutorly.Application/Commands/CreateCategory.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Tutorly.Application.Interfaces;
+
+namespace Tutorly.Application.Commands
+{
+    public class CreateCategory : ICommand
+    {
+        public Guid Id { get; }
+
+        public string Name { get; set; }    
+
+        public CreateCategory(string name)
+        {
+            Id = Guid.NewGuid();
+            Name = name;
+        }
+    }
+}
---
+++ Tutorly.Application/Dtos/CreateDtos/CreateCategoryDto.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tutorly.Application.Dtos.CreateDtos
+{
+    public class CreateCategoryDto
+    {
+        public CreateCategoryDto(string name)
+        {
+            Name = name;
+        }
+        public string Name { get; set; }    
+    }
+}
