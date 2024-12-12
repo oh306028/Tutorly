c7ad360efa1d04eaa209df9068dd62f9d813b622
Oskar <hamerlao@gmail.com>
2024-12-12 23:29:19 +0100

    created category createDto validator; validates name and if category is already in db

---
+++ Tutorly.Application/Validators/CreateCategoryDtoValidator.cs
@@ -0,0 +1,37 @@
+﻿using FluentValidation;
+using FluentValidation.Results;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Tutorly.Application.Dtos.CreateDtos;
+using Tutorly.Application.Interfaces;
+using Tutorly.Domain.Models;
+
+namespace Tutorly.Application.Validators
+{
+    public class CreateCategoryDtoValidator : AbstractValidator<CreateCategoryDto>
+    {
+
+        public CreateCategoryDtoValidator(IRepository<Category> categoryRepository)
+        {
+
+
+            RuleFor(n => n.Name)
+                .NotEmpty()
+                .MaximumLength(30);
+
+            RuleFor(n => n.Name)
+                .Custom((name, context) =>
+                {
+                    var createdCategory = categoryRepository.GetBy(n => n.Name == name);
+
+                    if (createdCategory != null)
+                        context.AddFailure("Category already exists");
+                });
+
+        }
+
+    }
+}
