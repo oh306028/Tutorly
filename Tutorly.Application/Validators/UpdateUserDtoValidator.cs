8f08b12da9148e88f646a7fd78b16d7427ef8603
Oskar <hamerlao@gmail.com>
2024-12-13 13:43:47 +0100

    created validator for updating user data

---
+++ Tutorly.Application/Validators/UpdateUserDtoValidator.cs
@@ -0,0 +1,31 @@
+﻿using FluentValidation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Tutorly.Application.Dtos.CreateDtos;
+
+namespace Tutorly.Application.Validators
+{
+    public class UpdateUserDtoValidator : AbstractValidator<UpdateUserDto>
+    {
+        public UpdateUserDtoValidator()
+        {
+            RuleFor(n => n.FirstName)
+                .MaximumLength(15)
+                .NotEmpty();
+                
+
+            RuleFor(n => n.LastName)
+               .MaximumLength(20)
+               .NotEmpty();
+
+            RuleFor(n => n.Email)
+             .MaximumLength(30)
+             .EmailAddress()
+             .NotEmpty();
+
+        }
+    }
+}
