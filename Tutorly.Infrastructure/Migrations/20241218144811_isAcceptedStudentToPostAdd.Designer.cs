b140ff5e3bf210f72a36433d88c920dce88302e8
Oskar <hamerlao@gmail.com>
2024-12-18 19:50:15 +0100

    added new migrations for isAccepted property in the PostStudent join table

--- Tutorly.Infrastructure/Migrations/20241218144811_isAcceptedStudentToPostAdd.Designer.cs
+++ Tutorly.Infrastructure/Migrations/20241218144811_isAcceptedStudentToPostAdd.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Tutorly.Infrastructure;
 
@@ -11,9 +12,10 @@ using Tutorly.Infrastructure;
 namespace Tutorly.Infrastructure.Migrations
 {
     [DbContext(typeof(TutorlyDbContext))]
-    partial class TutorlyDbContextModelSnapshot : ModelSnapshot
+    [Migration("20241218144811_isAcceptedStudentToPostAdd")]
+    partial class isAcceptedStudentToPostAdd
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ Tutorly.Infrastructure/Migrations/20241218144811_isAcceptedStudentToPostAdd.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Tutorly.Infrastructure.Migrations
+{
+    public partial class isAcceptedStudentToPostAdd : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<bool>(
+                name: "IsAccepted",
+                table: "PostsStudents",
+                type: "bit",
+                nullable: false,
+                defaultValue: false);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "IsAccepted",
+                table: "PostsStudents");
+        }
+    }
+}
