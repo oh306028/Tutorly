c09afaac407a9aa1c1af6c1aec17bf31b835f1f3
Oskar <hamerlao@gmail.com>
2024-12-18 19:49:31 +0100

    added handler and command for accepting students to the post by tutor

---
+++ Tutorly.Application/Commands/AcceptStudent.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Tutorly.Application.Interfaces;
+
+namespace Tutorly.Application.Commands
+{
+    public class AcceptStudent : ICommand
+    {
+        public Guid Id { get; }
+        public int PostId { get; }
+        public int StudentId { get; }
+        public bool IsAccepted { get; set; }     
+
+        public AcceptStudent(int postId, int studentId,  bool isAccepted)
+        {
+            Id = Guid.NewGuid();
+            PostId = postId;
+            StudentId = studentId;
+            IsAccepted = isAccepted;
+        }
+
+    }
+}
---
+++ Tutorly.Application/Handlers/AcceptStudentHandler.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Tutorly.Application.Commands;
+using Tutorly.Application.Interfaces;
+using Tutorly.Domain.Models;
+using Tutorly.Domain.ModelsExceptions;
+
+namespace Tutorly.Application.Handlers
+{
+    public class AcceptStudentHandler : IHandler<AcceptStudent>
+    {
+        private readonly IRepository<Post> _postRepository;
+        private readonly IRepository<Student> _studentRepository;
+
+        public AcceptStudentHandler(IRepository<Post> postRepository, IRepository<Student> studentRepository)
+        {
+            _postRepository = postRepository;
+            _studentRepository = studentRepository;
+        }
+        public async Task Handle(AcceptStudent command)
+        {
+            if (!command.IsAccepted)           
+                return;
+            
+            var post = await _postRepository.GetByIdAsync(command.PostId);
+
+            if (post is null)
+                throw new NotFoundException("Post not found");
+
+            var student = await _studentRepository.GetByIdAsync(command.StudentId);
+
+            if (student is null)
+                throw new NotFoundException("Post not found");
+
+            if (post.Students == null || !post.Students.Any())
+                throw new InvalidOperationException("Post does not have any students.");
+
+            post.AcceptStudent(student);
+
+            await _postRepository.UpdateAsync(post);
+            
+
+        }
+    }
+}
