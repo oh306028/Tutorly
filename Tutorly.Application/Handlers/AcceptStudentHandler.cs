fa0205ffc4f32a161cf6771817dc8aed99a71ab1
Oskar <hamerlao@gmail.com>
2024-12-19 20:29:54 +0100

    refactored for the resourceAvaibility requirement, now authorize the endpoint

--- Tutorly.Application/Handlers/AcceptStudentHandler.cs
+++ Tutorly.Application/Handlers/AcceptStudentHandler.cs
@@ -1,12 +1,15 @@
-﻿using System;
+﻿using Microsoft.AspNetCore.Authorization;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Tutorly.Application.Authorization;
 using Tutorly.Application.Commands;
 using Tutorly.Application.Interfaces;
 using Tutorly.Domain.Models;
 using Tutorly.Domain.ModelsExceptions;
+using Tutorly.WebAPI.Services;
 
 namespace Tutorly.Application.Handlers
 {
@@ -14,11 +17,16 @@ namespace Tutorly.Application.Handlers
     {
         private readonly IRepository<Post> _postRepository;
         private readonly IRepository<Student> _studentRepository;
+        private readonly IAuthorizationService _authorizationService;
+        private readonly IUserContextService _userContextService;
 
-        public AcceptStudentHandler(IRepository<Post> postRepository, IRepository<Student> studentRepository)
+        public AcceptStudentHandler(IRepository<Post> postRepository, IRepository<Student> studentRepository,
+            IAuthorizationService authorizationService, IUserContextService userContextService)
         {
             _postRepository = postRepository;
             _studentRepository = studentRepository;
+            _authorizationService = authorizationService;
+            _userContextService = userContextService;
         }
         public async Task Handle(AcceptStudent command)
         {
@@ -30,14 +38,21 @@ namespace Tutorly.Application.Handlers
             if (post is null)
                 throw new NotFoundException("Post not found");
 
-            var student = await _studentRepository.GetByIdAsync(command.StudentId);
-
-            if (student is null)
-                throw new NotFoundException("Post not found");
-
             if (post.Students == null || !post.Students.Any())
                 throw new InvalidOperationException("Post does not have any students.");
 
+            var user = _userContextService.User;
+            var authorizationResult = await _authorizationService.AuthorizeAsync(user, post, new ResourceAvaibilityRequirement());
+
+            if (!authorizationResult.Succeeded)
+                throw new ForbidException("Cannot accept student that does not apply for that post");
+
+
+            var student = await _studentRepository.GetByIdAsync(command.StudentId);
+
+            if (student is null)
+                throw new NotFoundException("Student not found");
+    
             post.AcceptStudent(student);
 
             await _postRepository.UpdateAsync(post);
--- Tutorly.Application/Handlers/DeletePostHandler.cs
+++ Tutorly.Application/Handlers/DeletePostHandler.cs
@@ -1,30 +1,46 @@
-﻿using System;
+﻿using Microsoft.AspNetCore.Authorization;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Tutorly.Application.Authorization;
 using Tutorly.Application.Commands;
 using Tutorly.Application.Interfaces;
 using Tutorly.Domain.Models;
 using Tutorly.Domain.ModelsExceptions;
+using Tutorly.WebAPI.Services;
 
 namespace Tutorly.Application.Handlers
 {
     public class DeletePostHandler : IHandler<DeletePost>
     {
         private readonly IRepository<Post> _postRepository;
+        private readonly IAuthorizationService _authorizationService;
+        private readonly IUserContextService _userContextService;
 
-        public DeletePostHandler(IRepository<Post> postRepository)
+        public DeletePostHandler(IRepository<Post> postRepository, IAuthorizationService authorizationService, IUserContextService userContextService)
         {
             _postRepository = postRepository;
+           _authorizationService = authorizationService;
+           _userContextService = userContextService;
         }
 
         public async Task Handle(DeletePost command)
         {
+
             var postToDelete = await _postRepository.GetByIdAsync(command.PostId);
 
             if (postToDelete is null)
                 throw new NotFoundException("Post not found");
+
+            var user = _userContextService.User;
+
+            var authorizationResult = await _authorizationService.AuthorizeAsync(user, postToDelete, new ResourceAvaibilityRequirement());
+
+            if (!authorizationResult.Succeeded)
+                throw new ForbidException("Cannot delete post that does not belong to that tutor");
+
 
             await _postRepository.DeleteAsync(postToDelete);
 
