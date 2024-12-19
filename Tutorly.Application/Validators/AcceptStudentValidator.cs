d4cc38ca77e89d3ac77e4d6dbb825e1fd66f1ea5
Oskar <hamerlao@gmail.com>
2024-12-19 20:38:14 +0100

    added validator for accepting student to the post

---
+++ Tutorly.Application/Validators/AcceptStudentValidator.cs
@@ -0,0 +1,22 @@
+﻿using FluentValidation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Tutorly.Application.Dtos.Params;
+
+namespace Tutorly.Application.Validators
+{
+    public class AcceptStudentValidator : AbstractValidator<AcceptStudentParams>
+    {
+        public AcceptStudentValidator()
+        {
+            RuleFor(s => s.StudentId)
+                .NotEmpty();
+
+            RuleFor(a => a.isAccepted)
+                .NotEmpty();
+        }
+    }
+}
