522eb369920b42a56ebc0b482c05dd24e830ea94
Oskar <hamerlao@gmail.com>
2024-12-20 11:16:31 +0100

    added new dto for decideStduent application as a dto not a query param

---
+++ Tutorly.Application/Dtos/CreateDtos/DecideStudentApplicationDto.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tutorly.Application.Dtos.CreateDtos
+{
+    public class DecideStudentApplicationDto
+    {
+        public int StudentId { get; set; }
+        public bool IsAccepted { get; set; }    
+
+        public DecideStudentApplicationDto(int studentId, bool isAccepted)
+        {
+            StudentId = studentId;
+            IsAccepted = isAccepted;
+        }
+
+    }
+}
