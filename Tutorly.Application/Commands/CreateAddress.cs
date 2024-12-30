0fc05972b0f3e1c8e02fbb7b75cd251140fd6047
Oskar <hamerlao@gmail.com>
2024-12-30 18:53:32 +0100

    created createAddress command and handler

---
+++ Tutorly.Application/Commands/CreateAddress.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Tutorly.Application.Interfaces;
+using Tutorly.Domain.Models;
+
+namespace Tutorly.Application.Commands
+{
+    public class CreateAddress : ICommand
+    {
+        public Guid Id { get; }
+        public int PostId { get; set; }
+        public string City { get; set; }
+        public string Street { get; set; }
+        public string Number { get; set; }  
+        public CreateAddress(int postId, string city, string street, string number)
+        {
+            Id = Guid.NewGuid();
+            PostId = postId;
+            City = city;
+            Street = street;
+            Number = number;
+        }
+    }
+}
---
+++ Tutorly.Application/Handlers/CreateAddressHandler.cs
@@ -0,0 +1,59 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Tutorly.Application.Authorization;
+using Tutorly.Application.Commands;
+using Tutorly.Application.Interfaces;
+using Tutorly.Domain.Models;
+using Tutorly.Domain.ModelsExceptions;
+using Tutorly.WebAPI.Services;
+
+namespace Tutorly.Application.Handlers
+{
+    public class CreateAddressHandler : IHandler<CreateAddress>
+    {
+        private readonly IRepository<Address> _addressRepository;
+        private readonly IRepository<Post> _postRepository;
+        private readonly IAuthorizationService _authorizationService;
+        private readonly IUserContextService _userContextService;
+
+        public CreateAddressHandler(IRepository<Address> addressRepository, IRepository<Post> postRepository, IAuthorizationService authorizationService,
+            IUserContextService userContextService)
+        {
+            _addressRepository = addressRepository;
+            _postRepository = postRepository;
+            _authorizationService = authorizationService;
+            _userContextService = userContextService;
+        }
+
+        public async Task Handle(CreateAddress command)
+        {
+            var post = await _postRepository.GetByIdAsync(command.PostId);
+
+            if (post is null)
+                throw new NotFoundException("Post not found");
+
+            var user = _userContextService.User;
+            var authenticationResult = await _authorizationService.AuthorizeAsync(user, post, new ResourceAvaibilityRequirement());
+
+            if (!authenticationResult.Succeeded)
+                throw new ForbidException("Cannot add address to the post that current user is not owner of");
+
+            var address = new Address()
+            {
+                City = command.City,
+                Street = command.Street,
+                Number = command.Number
+            };
+
+
+            await _addressRepository.AddAsync(address);
+
+
+        }
+
+    }
+}
