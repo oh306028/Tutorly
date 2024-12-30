823fd0f01a7cad71796d430c75df8cb53043bc73
Oskar <hamerlao@gmail.com>
2024-12-30 18:54:15 +0100

    added new endpoint for retreiving address and creating new, also service methods created

---
+++ Tutorly.WebAPI/Controllers/AddressController.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Tutorly.Application.Dtos.CreateDtos;
+using Tutorly.Application.Dtos.DisplayDtos;
+using Tutorly.WebAPI.Services;
+
+namespace Tutorly.WebAPI.Controllers
+{
+    [Route("api/posts")]
+    [ApiController]
+    public class AddressController : ControllerBase
+    {
+        private readonly IAddressService _addressService;
+
+        public AddressController(IAddressService addressService)
+        {
+            _addressService = addressService;
+        }
+
+        [HttpGet("{id}/address")]
+        public async Task<ActionResult<AddressDto>> GetPostAddress([FromRoute]int id)
+        {
+            var result = await _addressService.GetPostAddressAsync(id); 
+            return Ok(result);
+        }
+
+        [HttpPost("{id}/address")]
+        public async Task<ActionResult> CreateAddressToPost([FromRoute]int postId, [FromBody]CreateAddressDto dto)
+        {
+            await _addressService.CreateAddressAsync(postId, dto);
+            return Created("api/posts", null);
+        }
+
+    }
+}
---
+++ Tutorly.WebAPI/Services/AddressService.cs
@@ -0,0 +1,53 @@
+﻿using AutoMapper;
+using Tutorly.Application.Commands;
+using Tutorly.Application.Dtos.CreateDtos;
+using Tutorly.Application.Dtos.DisplayDtos;
+using Tutorly.Application.Handlers;
+using Tutorly.Application.Interfaces;
+using Tutorly.Application.Queries;
+using Tutorly.Domain.Models;
+using Tutorly.Domain.ModelsExceptions;
+
+namespace Tutorly.WebAPI.Services
+{
+    public interface IAddressService
+    {
+        Task<AddressDto> GetPostAddressAsync(int id);
+        Task CreateAddressAsync(int postId, CreateAddressDto dto);
+    }
+
+
+
+    public class AddressService : IAddressService
+    {
+        private readonly IQueryHandler<GetAddress, Address> _getAddressQueryHandler;
+        private readonly IMapper _mapper;
+        private readonly IHandler<CreateAddress> _createAddressHandler;
+
+        public AddressService(IQueryHandler<GetAddress, Address> getAddressQueryHandler, IMapper mapper, IHandler<CreateAddress> createAddressHandler)
+        {
+            _getAddressQueryHandler = getAddressQueryHandler;
+            _mapper = mapper;
+            _createAddressHandler = createAddressHandler;
+        }
+
+        public async Task<AddressDto> GetPostAddressAsync(int id)   
+        {
+            var query = new GetAddress(id);
+            var address = await _getAddressQueryHandler.HandleAsync(query);
+
+            var result = _mapper.Map<AddressDto>(address);
+
+            return result;
+
+        }
+
+        public async Task CreateAddressAsync(int postId, CreateAddressDto dto)
+        {
+            var command = new CreateAddress(postId, dto.City, dto.Street, dto.Number);
+            await _createAddressHandler.Handle(command);
+        }
+
+
+    }
+}
