0a8ead58ee2ba24ec0d79d3c4562479e8c1573a9
Oskar <hamerlao@gmail.com>
2024-12-30 18:55:24 +0100

    createAddress validator added

---
+++ Tutorly.Application/Validators/CreateAddressDtoValidator.cs
@@ -0,0 +1,28 @@
+﻿using FluentValidation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Tutorly.Application.Dtos.CreateDtos;
+
+namespace Tutorly.Application.Validators
+{
+    public class CreateAddressDtoValidator : AbstractValidator<CreateAddressDto>
+    {
+        public CreateAddressDtoValidator()
+        {
+            RuleFor(c => c.City)
+                .NotEmpty()
+                .MaximumLength(15);
+
+            RuleFor(s => s.Street)
+                .NotEmpty()
+                .MaximumLength(20);
+
+            RuleFor(n => n.Number)
+                .NotEmpty()
+                .MaximumLength(10);
+        }
+    }
+}
